b2db55f8e0f695c5bd67e26d7fb7a03ca0273f17
Tom Beynon <tbeynon@rawnet.com>
2013-04-09 13:55:23 +0100

    Initial import. Basic app commands working

---
+++ alfred_pow.rb
@@ -0,0 +1,69 @@
+require 'rexml/document'
+require File.dirname(__FILE__) + '/lib/pow_app.rb'
+
+class AlfredPow
+  
+  def self.create(source_path)
+    app = PowApp.new({
+      :name => File.basename(source_path),
+      :source_path => source_path
+    })
+    if app.create!
+      puts "#{app.name}.dev created successfully"
+    else
+      puts "Unable to create app"
+    end
+  end
+  
+  def self.browse(pow_path)
+    if app = PowApp.find(pow_path)
+      app.browse
+      puts "#{app.name}.dev opened successfully"
+    else
+      puts "Unable to open app"
+    end
+  end
+  
+  def self.destroy(pow_path)
+    if app = PowApp.find(pow_path)
+      if app.destroy
+        puts "#{app.name} destroyed successfully"
+      else
+        puts "Unable to destroy #{app.name}"
+      end
+    else
+      puts "Couldn't find that app"
+    end
+  end
+  
+  def self.restart(pow_path)
+    if app = PowApp.find(pow_path)
+      app.restart
+      puts "#{app.name}.dev restarted successfully"
+    else
+      puts "That app does not exist"
+    end
+  end
+  
+  def self.xip(pow_path)
+    if app = PowApp.find(pow_path)
+      puts app.xip_url
+    else
+      puts "That app does not exist"
+    end
+  end
+
+  def self.list(keyword)
+    found_apps = PowApp.search(keyword)
+    doc = REXML::Document.new
+    items = doc.add_element 'items'
+    found_apps.each do |app|
+      item = items.add_element('item', {'uid' => app.name, 'arg' => app.path})
+      item.add_element('title').add_text(app.name)
+      item.add_element('subtitle').add_text(app.url)
+      item.add_element('icon', {'type' => ''})
+    end
+    puts doc.to_s
+  end
+
+end
---
+++ lib/pow_app.rb
@@ -0,0 +1,85 @@
+require 'fileutils'
+require 'socket'
+
+class PowApp
+  
+  attr_reader :name, :path, :source_path, :url, :xip_url
+  
+  POW_PATH = File.expand_path('~/.pow')
+  
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @path = attributes[:path]
+    @source_path = attributes[:source_path]
+    @url = "http://#{name}.dev"
+    @xip_url = "http://#{name}.#{PowApp.local_ip}.xip.io"
+  end
+  
+  def create!
+    unless name.nil? || source_path.nil?
+      # Create the pow link
+      FileUtils.ln_s(source_path, "#{POW_PATH}/#{name}")
+    end
+    true
+  rescue Errno::EEXIST
+    true
+  rescue
+    false
+  end
+  
+  def browse
+    %x{open #{url}}
+  end
+  
+  def destroy
+    if File.exists?(path)
+      FileUtils.rm(path)
+      true
+    else
+      false
+    end
+  end
+  
+  def restart
+    FileUtils.mkdir_p("#{path}/tmp")
+    FileUtils.touch("#{path}/tmp/restart.txt")
+  end
+  
+  def self.find(pow_path)
+    if File.directory? pow_path
+      pow_app = PowApp.new({
+        :name => File.basename(pow_path),
+        :path => pow_path
+      })
+    end
+    pow_app
+  end
+  
+  def self.search(keyword)
+    apps = []
+    found_apps = all_pow_apps.select { |app| app.match keyword }
+    found_apps.each do |app|
+      apps << find(File.expand_path(app, POW_PATH))
+    end
+    apps
+  end
+
+  def self.all_pow_apps 
+    @all_apps ||= begin
+      Dir.chdir(POW_PATH)
+      Dir.glob('*')
+    end
+  end
+  
+  def self.local_ip
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+    UDPSocket.open do |s|
+      # We're using Google's IP, but we don't actually make a connection or send anything
+      s.connect '64.233.187.99', 1
+      s.addr.last
+    end
+  ensure
+    Socket.do_not_reverse_lookup = orig
+  end
+  
+end
