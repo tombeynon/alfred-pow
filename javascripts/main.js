a088552abde6fc474d730f131483cb8bfa257e64
Tom Beynon <tbeynon@rawnet.com>
2013-04-11 15:50:43 +0100

    Some event tracking

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+$("document").ready(function(){
+	
+	$('a.download').on('click', function(event) {
+		target = $(this).attr("rel");
+		if(target != "" && target != undefined) ga('send', 'event', 'download', target);
+	});
+	
+	$('a.view').on('click', function(event) {
+		target = $(this).attr("rel");
+		if(target != "" && target != undefined) ga('send', 'event', 'view', target);
+	});
+});
