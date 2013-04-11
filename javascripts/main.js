88f25a4892d70c944629f9365f609ffceb1206c0
Tom Beynon <tbeynon@rawnet.com>
2013-04-11 16:43:35 +0100

    Added labels to events

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,13 @@ $("document").ready(function(){
 	
 	$('a.download').on('click', function(event) {
 		target = $(this).attr("rel");
-		if(target != "" && target != undefined) ga('send', 'event', 'download', target);
+		url = $(this).attr("href");
+		if(target != "" && target != undefined) ga('send', 'event', 'download', target, url);
 	});
 	
 	$('a.view').on('click', function(event) {
 		target = $(this).attr("rel");
-		if(target != "" && target != undefined) ga('send', 'event', 'view', target);
+		url = $(this).attr("href");
+		if(target != "" && target != undefined) ga('send', 'event', 'view', target, url);
 	});
 });
