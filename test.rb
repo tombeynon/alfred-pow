5277bcd35816e2bdb0905ca56d92c7c9c6a2856c
Tom Beynon <tbeynon@rawnet.com>
2013-04-11 10:02:21 +0100

    First version of Github page. Analytics, meta, instructions and imagery

---
+++ test.rb
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + '/alfred_pow.rb'
+
+AlfredPow.create("/Users/tombeynon/Sites/wise_buddah")
+AlfredPow.list("wise")
+AlfredPow.xip("/Users/tombeynon/.pow/wise_buddah")
+AlfredPow.destroy("/Users/tombeynon/.pow/wise_buddah")
